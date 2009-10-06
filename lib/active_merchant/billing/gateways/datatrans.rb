b7a75e2c4346021ea92d55ee64ef1a6a6e524ca2
Roman Simecek <roman.simecek@screenconcept.ch>
2009-10-06 09:59:34 +0200

    settlement added

--- lib/active_merchant/billing/gateways/datatrans.rb
+++ lib/active_merchant/billing/gateways/datatrans.rb
@@ -1,26 +1,36 @@
+require 'xml'
+
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
-    class DatatranGateway < Gateway
-      TEST_URL = 'https://example.com/test'
-      LIVE_URL = 'https://example.com/live'
+    class DatatransGateway < Gateway
+      TEST_URL = 'http://pilot.datatrans.biz/upp/jsp/XML_processor.jsp'
+      LIVE_URL = 'http://payment.datatrans.biz/upp/jsp/XML_processor.jsp'
+
+      # Datatrans status success code
+      DATATRANS_STATUS_SUCCESS = 'trxStatus = response'
+      # Datatrans status error code
+      DATATRANS_ERROR_SUCCESS = 'trxStatus = error'
       
       # The countries the gateway supports merchants from as 2 digit ISO country codes
-      self.supported_countries = ['US']
+      self.supported_countries = ['CH']
       
       # The card types supported by the payment gateway
-      self.supported_cardtypes = [:visa, :master, :american_express, :discover]
+      self.supported_cardtypes = [:visa, :master, :american_express, :diners_club]
+
+      # the default currency
+      self.default_currency = 'CHF'
       
       # The homepage URL of the gateway
-      self.homepage_url = 'http://www.example.net/'
+      self.homepage_url = 'http://www.datatrans.ch'
       
       # The name of the gateway
-      self.display_name = 'New Gateway'
+      self.display_name = 'DataTrans'
       
       def initialize(options = {})
-        #requires!(options, :login, :password)
+        requires!(options, :login, :password)
         @options = options
         super
-      end  
+      end
       
       def authorize(money, creditcard, options = {})
         post = {}
@@ -43,34 +53,86 @@ module ActiveMerchant #:nodoc:
       end                       
     
       def capture(money, authorization, options = {})
-        commit('capture', money, post)
-      end
-    
-      private                       
-      
-      def add_customer_data(post, options)
+        doc = ""
+        xml = Builder::XmlMarkup.new(:target => doc, :indent => 2)
+        xml.instruct!
+        xml.paymentService(:version => '1'){
+          xml.body(:merchantId => options[:merchant_id], :testOnly => test? ? 'yes' : 'no' ){
+            xml.transaction(:refno => options[:refno]){
+              xml.request{
+                xml.amount(
+                  money
+                )
+                xml.currency(
+                  self.default_currency || options[:currency]
+                )
+                xml.authorizationCode(
+                  authorization
+                )
+              }
+            }
+          }
+        }
+        url = URI.parse(test? ? TEST_URL : LIVE_URL)
+        headers = {"Content-Type" => "text/xml"}
+        h = Net::HTTP.new(url.host, url.port)
+        h.use_ssl = false
+        resp = h.post(url.path, doc, headers)
+        commit(resp)
       end
 
-      def add_address(post, creditcard, options)      
+      private
+      
+      def commit(response)
+        source = XML::Document.string(response.body)
+        status = source.find('//@trxStatus')
+        if status.to_a?equal(DATATRANS_STATUS_SUCCESS)
+          Response.new(success, message, params, options)
+        end
+#        Response.new(response[:status] == DATACASH_SUCCESS, response[:reason], response,
+#          :test => test?,
+#          :authorization => "#{response[:datacash_reference]};#{response[:authcode]};#{response[:ca_reference]}"
+#        )
+#        Response.new(true, 'OK', test[:reason => 'test'], :test => test?)
       end
 
-      def add_invoice(post, options)
-      end
-      
-      def add_creditcard(post, creditcard)      
-      end
-      
-      def parse(body)
-      end     
-      
-      def commit(action, money, parameters)
+      def payment_method(creditcard)
+        if(creditcard.type == 'visa')
+          return 'VIS'
+        elsif(creditcard.type == 'master_card')
+          return 'ECA'
+        elsif(creditcard.type == 'american_express')
+          return 'AMX'
+        elsif(creditcard.type == 'diners_club')
+          return 'DIN'
+        else
+          return 'ERROR'
+        end
       end
 
-      def message_from(response)
-      end
-      
-      def post_data(action, parameters = {})
-      end
+#      def add_customer_data(post, options)
+#      end
+#
+#      def add_address(post, creditcard, options)
+#      end
+#
+#      def add_invoice(post, options)
+#      end
+#
+#      def add_creditcard(post, creditcard)
+#      end
+#
+#      def parse(body)
+#      end
+#
+#      def commit(action, money, parameters)
+#      end
+#
+#      def message_from(response)
+#      end
+#
+#      def post_data(action, parameters = {})
+#      end
     end
   end
 end
--- test/unit/gateways/datatrans_test.rb
+++ test/unit/gateways/datatrans_test.rb
@@ -1,49 +1,103 @@
 require 'test_helper'
 
-class DatatranTest < Test::Unit::TestCase
+class DatatransTest < Test::Unit::TestCase
   def setup
-    @gateway = DatatranGateway.new(
+    @gateway = DatatransGateway.new(
                  :login => 'login',
                  :password => 'password'
                )
 
-    @credit_card = credit_card
+    @visa_credit_card = CreditCard.new(
+                     :month      => '12',
+                     :year       => '2010',
+                     :type       => 'visa',
+                     :number     => '4242424242424242'
+                   )
+    @mastercard_credit_card = CreditCard.new(
+                     :month      => '12',
+                     :year       => '2010',
+                     :type       => 'master_card',
+                     :number     => '5200000000000007'
+                   )
+    @amercianexpress_credit_card = CreditCard.new(
+                     :month      => '12',
+                     :year       => '2010',
+                     :type       => 'american_express',
+                     :number     => '375811111111115'
+                   )
+    @dinersclub_credit_card = CreditCard.new(
+                     :month      => '12',
+                     :year       => '2010',
+                     :type       => 'diners_club',
+                     :number     => '36168002586009'
+                   )
+
     @amount = 100
     
     @options = { 
-      :order_id => '1',
+      :order_id => generate_unique_id,
       :billing_address => address,
-      :description => 'Store Purchase'
+      :description => 'Store Purchase',
+      :merchant_id => '1000011907',
+      :refno => '23232301'
     }
   end
-  
-  def test_successful_purchase
-    @gateway.expects(:ssl_post).returns(successful_purchase_response)
-    
-    assert response = @gateway.purchase(@amount, @credit_card, @options)
-    assert_instance_of 
-    assert_success response
-    
-    # Replace with authorization number from the successful response
-    assert_equal '', response.authorization
-    assert response.test?
+
+  def test_supported_countries
+    assert_equal ['CH'], DatatransGateway.supported_countries
   end
 
-  def test_unsuccessful_request
-    @gateway.expects(:ssl_post).returns(failed_purchase_response)
-    
-    assert response = @gateway.purchase(@amount, @credit_card, @options)
-    assert_failure response
-    assert response.test?
+  def test_supported_card_types
+    assert_equal [ :visa, :master, :american_express, :diners_club ], DatatransGateway.supported_cardtypes
   end
 
-  private
+#  def test_payment_method_visa_card
+#    assert_equal 'VIS', @gateway.payment_method(@visa_credit_card)
+#  end
+#
+#  def test_payment_method_mastercard_card
+#    assert_equal 'ECA', @gateway.payment_method(@mastercard_credit_card)
+#  end
+#
+#  def test_payment_method_amercian_express_card
+#    assert_equal 'AMX', @gateway.payment_method(@amercianexpress_credit_card)
+#  end
+#
+#  def test_payment_method_dinersclub_card
+#    assert_equal 'DIN', @gateway.payment_method(@dinersclub_credit_card)
+#  end
+
+#  def test_capture
+#    @gateway.capture(1000, 724607995, @options)
+#  end
   
-  # Place raw successful response from gateway here
-  def successful_purchase_response
-  end
-  
-  # Place raw failed response from gateway here
-  def failed_purcahse_response
-  end
+#  def test_successful_purchase
+#    @gateway.expects(:ssl_post).returns(successful_purchase_response)
+#
+#    assert response = @gateway.purchase(@amount, @credit_card, @options)
+#    assert_instance_of
+#    assert_success response
+#
+#    # Replace with authorization number from the successful response
+#    assert_equal '', response.authorization
+#    assert response.test?
+#  end
+#
+#  def test_unsuccessful_request
+#    @gateway.expects(:ssl_post).returns(failed_purchase_response)
+#
+#    assert response = @gateway.purchase(@amount, @credit_card, @options)
+#    assert_failure response
+#    assert response.test?
+#  end
+#
+#  private
+#
+#  # Place raw successful response from gateway here
+#  def successful_purchase_response
+#  end
+#
+#  # Place raw failed response from gateway here
+#  def failed_purcahse_response
+#  end
 end
