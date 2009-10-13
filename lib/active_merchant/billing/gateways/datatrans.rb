ff271df77ea6ab7380d2af9ae130bb4a57f7ee2c
Roman Simecek <roman.simecek@screenconcept.ch>
2009-10-13 13:39:26 +0200

    function void added and removed some bugs

--- lib/active_merchant/billing/gateways/datatrans.rb
+++ lib/active_merchant/billing/gateways/datatrans.rb
@@ -102,32 +102,80 @@ module ActiveMerchant #:nodoc:
         commit(response)
       end
 
+      # Cancels the transaction
+      #
+      # ==== Parameters
+      # * <tt>authorization</tt> - The authorization code received from the authorization.
+      # * <tt>options</tt>
+      #   * <tt>:refno</tt> - The Reference Number of the transaction.
+      #   * <tt>:amount</tt> - The amount of the transaction
+      #   * <tt>:currency</tt> - Optional the Currency of the transaction, default CHF.
+      def void(authorization, options = {})
+        doc = ""
+        xml = REXML::Document.new('<?xml version="1.0" encoding="UTF-8" ?>')
+        root = xml.add_element "paymentService", {"version" => "1"}
+        body = root.add_element "body", {"merchantId" => @options[:merchant_id], "testOnly" => test? ? 'yes' : 'no' ""}
+        transaction = body.add_element "transaction", {"refno" => options[:refno]}
+        request = transaction.add_element "request"
+        amount = request.add_element "amount"
+        amount.text = options[:amount].to_s
+        currency = request.add_element "currency"
+        currency.text = self.default_currency.to_s || options[:currency].to_s
+        authorization_code = request.add_element "authorizationCode"
+        authorization_code.text = authorization.to_s
+        reqtype = request.add_element "reqtype"
+        reqtype.text = "DOA"
+        url = URI.parse(test? ? TEST_URL : LIVE_URL)
+        headers = {"Content-Type" => "text/xml"}
+        h = Net::HTTP.new(url.host, url.port)
+        h.use_ssl = false
+        xml.write(doc, 0)
+        resp = h.post(url.path, doc, headers)
+        response = parse(resp.body)
+        commit(response)
+      end
+
       private
 
       def parse(data)
-        puts data
         response = {}
         source = REXML::Document.new(data)
         root = source.root
         body = root.get_elements("body").first
-        error_code = "" || body.get_elements("errorCode").first.get_text
-        error_message = "" || body.get_elements("errorMessage").first.get_text
-        error_detail = "" || body.get_elements("errorDetail").first.get_text
-        ref_no = "" || body.get_elements("transaction").first.attributes["refno"]
+        transactin_error = root.get_elements("body/transaction/error").first
+        error = root.get_elements("body/error").first
+        if transactin_error == nil && error == nil
+          message = root.get_elements("body/transaction/response/responseMessage").first.get_text
+          detail = "No details"
+          code = root.get_elements("body/transaction/response/responseCode").first.get_text
+          ref_no = root.get_elements("body/transaction").first.attributes["refno"]
+        elsif error != nil
+          message = root.get_elements("body/error/errorMessage").first.get_text
+          detail = root.get_elements("body/error/errorDetail").first.get_text
+          code = root.get_elements("body/error/errorCode").first.get_text
+          ref_no = root.get_elements("body/transaction").first.attributes["refno"]
+        else
+          message = root.get_elements("body/transaction/error/errorMessage").first.get_text
+          detail = root.get_elements("body/transaction/error/errorDetail").first.get_text
+          code = root.get_elements("body/transaction/error/errorCode").first.get_text
+          ref_no = root.get_elements("body/transaction").first.attributes["refno"]
+        end
         response = {:status => body.attributes["status"].to_s,
                     :trx_status => body.get_elements("transaction").first.attributes["trxStatus"].to_s,
-                    :error_code => error_code,
-                    :reason => error_message,
-                    :error_detail => error_detail,
+                    :code => code,
+                    :message => message,
+                    :detail => detail,
                     :ref_no => ref_no
                    }
         response
       end
       
       def commit(response)
-        Response.new(response[:status].to_s.eql?(DATATRANS_STATUS_SUCCESS) && response[:trx_status].to_s.eql?(DATATRANS_TRXSTATUS_SUCCESS), response[:reason], response,
-          :test => test?
-        )
+        Response.new(response[:status].to_s.eql?(DATATRANS_STATUS_SUCCESS) && response[:trx_status].to_s.eql?(DATATRANS_TRXSTATUS_SUCCESS),
+                     response[:message],
+                     response,
+                     :test => test?
+                    )
       end
 
       def payment_method(creditcard)
